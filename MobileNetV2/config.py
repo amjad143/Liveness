55f876055ec88eadc0fe6a732e79744e26a58b43
Amjad Khan <55101399+amjad143@users.noreply.github.com>
2022-11-23 09:19:18 +0530

    Add files via upload

---
+++ MobileNetV2/config.py
@@ -0,0 +1,10 @@
+# parameters
+counter_ok_questions = 0
+counter_ok_consecutives = 0
+limit_consecutives = 1
+limit_questions = 5
+counter_try = 0
+limit_try = 50
+CEF_COUNTER = 0
+CLOSED_EYES_FRAME = 3
+counter_time_gap = 50
